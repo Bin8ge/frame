f6452f346367e747f8fbc75490fa75ff0a41eaf4
bingo <3362235297@qq.com>
2020-07-13 18:11:21 +0800

    kernel

---
+++ app/Http/Kernel.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http;
+use LaravelStar\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+
+}
--- public/index.php
+++ public/index.php
@@ -1,7 +1,19 @@
 <?php
+require_once '../vendor/autoload.php';
+
 use LaravelStar\Foundation\Application;
 
-//$app = new Application($_ENV['APP_BASE_PATH'] ?? dirname(__DIR__));
+$app = new Application($_ENV['APP_BASE_PATH'] ?? dirname(__DIR__));
+
+// 处理http请求的
+$app->singleton(
+    \LaravelStar\Contracts\Http\Kernel::class,
+    \App\Http\Kernel::class
+);
+
+$kernel = $app->make(\LaravelStar\Contracts\Http\Kernel::class, [$app]);
+
+$response = $kernel->handle();
 
 
-var_dump(__DIR__);
+echo json_encode($app->make('config')->all());
